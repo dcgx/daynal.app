aeb418c18c808b8a76121d8280b4e311d7a4e1c0
dcastillog <diego.castilloguajardo@gmail.com>
2021-12-05 02:43:55 -0300

    firebase config file add meta.env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,7 +4,14 @@ import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase config
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
